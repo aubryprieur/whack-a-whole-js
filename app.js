e5bf815a1da38ca977c3070b08ed2e4b071b6ce7
aubry prieur <aubry.prieur@gmail.com>
2020-04-12 17:16:44 +0200

    Implemented cardArray sort

--- app.js
+++ app.js
@@ -52,6 +52,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   ]
 
+  cardArray.sort(() => 0.5 - Math.random())
+
   const grid = document.querySelector('.grid')
   const resultDisplay = document.querySelector('#result')
   var cardsChosen = []

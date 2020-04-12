3c58ac8b6b3986703efe14ba0495fbb2d5323f27
aubry prieur <aubry.prieur@gmail.com>
2020-04-12 16:43:47 +0200

    Setted up the game board

--- app.js
+++ app.js
@@ -1,3 +1,73 @@
-document.addEventListener('DomcontentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+
+  //card options
+  const cardArray = [
+    {
+      name: "cat",
+      img: "images/cat.jpg"
+    },
+    {
+      name: "cat",
+      img: "images/cat.jpg"
+    },
+    {
+      name: "diddley",
+      img: "images/diddley.jpg"
+    },
+    {
+      name: "diddley",
+      img: "images/diddley.jpg"
+    },
+    {
+      name: "homer",
+      img: "images/homer.jpg"
+    },
+    {
+      name: "homer",
+      img: "images/homer.jpg"
+    },
+    {
+      name: "lanfeust",
+      img: "images/lanfeust.png"
+    },
+    {
+      name: "lanfeust",
+      img: "images/lanfeust.png"
+    },
+    {
+      name: "rtf",
+      img: "images/rtf.jpg"
+    },
+    {
+      name: "rtf",
+      img: "images/rtf.jpg"
+    },
+    {
+      name: "samurai",
+      img: "images/samurai.jpg"
+    },
+    {
+      name: "samurai",
+      img: "images/samurai.jpg"
+    }
+  ]
+
+  const grid = document.querySelector('.grid')
+
+  //Create the board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img')
+      card.setAttribute('src', 'images/rainbow.png')
+      card.setAttribute('data-id', i)
+      //card.addEventListener('click', flipcard)
+      grid.appendChild(card)
+    }
+  }
+
+  createBoard()
+
 
 })
+
+
